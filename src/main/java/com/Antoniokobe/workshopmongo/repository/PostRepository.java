9e7823e4848ea0c23471860fc0eb3a66fe65990b
antonioconceicao@outlook.com <antonioconceicao@outlook.com>
2025-10-11 12:08:50 -0300

    Consulta simples com query methods

--- src/main/java/com/Antoniokobe/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/Antoniokobe/workshopmongo/repository/PostRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.Antoniokobe.workshopmongo.domain.Post;
+import java.util.List;
+
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 	
 
